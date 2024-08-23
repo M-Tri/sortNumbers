d9f27008fdf9720266a40b606d00a64bc66f799f
MT <69726672+M-Tri@users.noreply.github.com>
2024-08-23 01:10:41 -0600

    Fix: BubbleSort algarithm.

--- sortNumbers/include/BubbleSort.h
+++ sortNumbers/include/BubbleSort.h
@@ -1,8 +1,10 @@
 #ifndef BUBBLESORT_H
 #define BUBBLESORT_H
-
 #include "PickSortingMethodInterface.h"
 
+//Worst-Case Time Complexity O(n^2).
+//Considered inefficient for large datasets.
+//Stable.
 class BubbleSort : public PickSortingMethodInterface {
     public:
         virtual void sort(std::vector<int>& numbers) override ;
--- sortNumbers/src/BubbleSort.cpp
+++ sortNumbers/src/BubbleSort.cpp
@@ -5,9 +5,9 @@
 void BubbleSort :: sort(std::vector<int>& numbers){
             size_t size = numbers.size() ;
             for(int i = 0 ; i < size - 1 ; i++){
-                for(int j = i + 1 ; j < size - i ; j++){
-                    if(numbers[i] > numbers[j]){
-                        std::swap(numbers[i], numbers[j]) ;
+                for(int j = 0 ; j < size - i - 1 ; j++){
+                    if(numbers[j] > numbers[j + 1]){
+                        std::swap(numbers[j], numbers[j + 1]) ;
                     }
                 }
             }
